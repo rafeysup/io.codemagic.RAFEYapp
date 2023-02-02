82f396431a4bf7ce084009602b2ee2f285161a40
Rafey <rafey.saleem@getcake.com>
2023-02-02 16:53:38 +0500

    Revert "Depedencies change"
    This reverts commit c0ae4dd7de49a0e358254e40c218453ec7ee163a.
    
    Revert "updated minsdkversion to 23"
    
    This reverts commit d277f74786fa92f95dd52a3378141c320088240b.
    
    Revert "removed extra plugin"
    
    This reverts commit aa5f935890ebbfb6505d8906d60737f8eb977967.
    
    Revert "firebase_core_web removed"
    
    This reverts commit 8cccf31bbd8d05351358c2b56989f94fd526d717.
    
    Revert "removed deprecated plugins"
    
    This reverts commit f04764a7e31c4800709faacc4fc7fa142036cc65.
    
    Revert "removed crashlytics"
    
    This reverts commit 3f9198efddff1ca9b433bb6b941cce2887076947.
    
    Revert "Changed again"
    
    This reverts commit 8e627bbf4904d18df2351e3c6a3cfab154642cbb.
    
    Revert "Added code for firebase"
    
    This reverts commit 2d63c500af68b2bce6054d60a894ce05b49e644b.
    
    Revert "Syntax issue resolved"
    
    This reverts commit 368dc6226f4b577078870fd9deee3d953929ffcb.
    
    Revert "updated apk package name in default config"
    
    This reverts commit 232ca12c485619ff59b7f7efacdc0717c631727a.
    
    Revert "Removed extra properties"
    
    This reverts commit 5627c7ab598005b4a08328436a40078a0516d219.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-void main() {    
-    runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
